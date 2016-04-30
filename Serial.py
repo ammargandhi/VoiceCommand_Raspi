c1a348a010398707766ab14754192611ec86d976
Ammar Gandhi <ammar.cma@gmail.com>
2016-04-30 22:08:44 +0530

    Update and rename templogger2.py to Serial.py

---
+++ Serial.py
@@ -0,0 +1,23 @@
+import serial
+import sys
+
+if sys.argv[1]=='one'
+  ser=Serial.serial('/dev/ttyUSB0',9600)
+  ser.write('a')
+  ser.close()
+  
+if sys.argv[1]=='two'
+  ser=Serial.serial('/dev/ttyUSB0',9600)
+  ser.write('b')
+  ser.close()
+  
+if sys.argv[1]=='three'
+  ser=Serial.serial('/dev/ttyUSB0',9600)
+  ser.write('c')
+  ser.close()
+  
+if sys.argv[1]=='four'
+  ser=Serial.serial('/dev/ttyUSB0',9600)
+  ser.write('d')
+  ser.close()
+        
